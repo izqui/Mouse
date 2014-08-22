04c85971a8bc571c0752e08bdb52bc65a00369c6
Jorge Izquierdo <jorge@izqui.me>
2014-08-22 15:51:22 -0400

    Initial

---
+++ Mouse/Channel.swift
@@ -0,0 +1,133 @@
+//From:
+
+import Foundation
+
+func go(routine: () -> ()) {
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), routine)
+}
+
+func go(routine: @autoclosure() -> ()) {
+    go(routine as () -> ())
+}
+
+infix operator  <- { associativity left }
+func <- (c: Chan, value: AnyObject?) { c.send(value) }
+func <- (inout value: AnyObject?, chan: Chan) { value = chan.recv() }
+
+prefix operator p<- {}
+prefix assignment func <- (inout chan: Chan) -> AnyObject? { return chan.recv() }
+
+class Chan {
+    class Waiter : NSObject {
+        enum Direction : Int {
+            case Receive = 0
+            case Send
+        }
+
+        let direction : Direction
+        var fulfilled : Bool = false
+        let sema : dispatch_semaphore_t = dispatch_semaphore_create(0)
+
+        var value : AnyObject? {
+        get {
+            if direction == .Receive {
+                fulfilled = true
+                dispatch_semaphore_signal(sema)
+            } else if !fulfilled {
+                dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER)
+            }
+            return _value
+        }
+        set(newValue) {
+            _value = newValue
+            if direction == .Send {
+                fulfilled = true
+                dispatch_semaphore_signal(sema)
+            } else if !fulfilled {
+                dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER)
+            }
+        }
+        }
+        var _value : AnyObject?
+
+        init(direction : Direction) {
+            self.direction = .Send
+        }
+    }
+
+    var lock : NSLock = NSLock()
+
+    let capacity : Int = Int.max
+    var buffer : [AnyObject?] = []
+    var sendQ : [Waiter] = []
+    var recvQ : [Waiter] = []
+
+    init (buffer:Int) {
+        self.capacity = buffer
+    }
+
+    var count : Int {
+        return buffer.count
+    }
+
+    func send(value: AnyObject?) {
+        lock.lock()
+
+        // see if we can immediately pair with a waiting receiver
+        if let recvW = removeWaiter(&recvQ) {
+            recvW.value = value
+            lock.unlock()
+            return
+        }
+
+        // if not, use the buffer if there's space
+        if self.buffer.count < self.capacity {
+            self.buffer.append(value)
+            lock.unlock()
+            return
+        }
+
+        // otherwise block until we can send
+        let sendW = Waiter(direction: .Send)
+        sendQ.append(sendW)
+        lock.unlock()
+        sendW.value = value
+    }
+
+    func recv() -> AnyObject? {
+        lock.lock()
+
+        // see if there's oustanding messages in the buffer
+        if buffer.count > 0 {
+            let value : AnyObject? = buffer.removeAtIndex(0)
+
+            // unblock waiting senders using buffer
+            if let sendW = removeWaiter(&sendQ) {
+                buffer.append(sendW.value)
+            }
+
+            lock.unlock()
+            return value
+        }
+
+        // if not, pair with any waiting senders
+        if let sendW = removeWaiter(&sendQ) {
+            lock.unlock()
+            return sendW.value
+        }
+
+        // otherwise, block until a message is available
+        let recvW = Waiter(direction: .Receive)
+        recvQ.append(recvW)
+        lock.unlock()
+
+        return recvW.value
+    }
+
+    func removeWaiter(inout waitQ : Array<Waiter>) -> Waiter? {
+        if waitQ.count > 0 {
+            return waitQ.removeAtIndex(0)
+        }
+        return nil
+    }
+}
---
+++ Mouse/ViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ViewController.swift
+//  Mouse
+//
+//  Created by Jorge Izquierdo on 22/08/14.
+//  Copyright (c) 2014 Jorge Izquierdo. All rights reserved.
+//
+
+import UIKit
+import CoreMotion
+
+class Canvas: UIView {
+    
+    var path = UIBezierPath()
+    
+    required init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.path.moveToPoint(self.center)
+        self.backgroundColor = UIColor.whiteColor()
+        
+    }
+    
+    func addPoint(p: CGPoint){
+        
+        self.path.addLineToPoint(p)
+        self.setNeedsDisplay()
+    }
+    
+    override func drawRect(rect: CGRect) {
+        
+        let context = UIGraphicsGetCurrentContext()
+        CGContextSetStrokeColorWithColor(context, UIColor.redColor().CGColor)
+        CGContextSetLineWidth(context, 1.0)
+        CGContextAddPath(context, self.path.CGPath)
+        CGContextStrokePath(context)
+    }
+}
+class ViewController: UIViewController {
+    
+    let motionManager = CMMotionManager()
+    var canvas: Canvas?
+    
+    override func viewDidLoad() {
+        
+        self.canvas = Canvas(frame: self.view.frame)
+        self.view.addSubview(self.canvas!)
+        
+        self.run()
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func run() {
+        
+        var ac = Chan(buffer:20)
+        var gy = Chan(buffer:20)
+        go {
+            self.accelerometer(ac)
+            self.gyroscope(gy)
+        }
+        
+        go {
+            for {
+                let acData = <-ac as CMAccelerometerData
+            }
+        }
+    }
+    
+    func accelerometer(cb: Chan) {
+        
+        var s0 = self.canvas!.path.currentPoint
+        var v0 = CGPoint(x: 0, y: 0)
+    
+        let t = CGFloat(1.0/20.0)
+        
+        if self.motionManager.accelerometerAvailable && !motionManager.accelerometerActive {
+            self.motionManager.accelerometerUpdateInterval = Double(t)
+            self.motionManager.startAccelerometerUpdatesToQueue (NSOperationQueue.mainQueue()) {
+                
+                (data: CMAccelerometerData!, error: NSError!) in
+                
+                self.canvas!.setNeedsDisplay()
+                
+                let a = CGPoint(x: CGFloat(data.acceleration.x)*10000, y: CGFloat(data.acceleration.y)*10000)
+                
+                let s = CGPoint(x: s0.x + v0.x*t + a.x*pow(t,2.0), y: s0.y + v0.y*t + a.y*pow(t,2.0))
+                v0 = CGPoint(x: v0.x + a.x * t, y: v0.y + a.y * t)
+                
+                self.canvas?.addPoint(s)
+                println("\(data.acceleration.x),\(data.acceleration.y)")
+                return
+            }
+        }
+    }
+    
+    func gyroscope(cb: Chan) {
+        
+        if self.motionManager.gyroAvailable && !motionManager.gyroActive {
+            self.motionManager.gyroUpdateInterval = 1.0/20.0
+            self.motionManager.startGyroUpdatesToQueue(NSOperationQueue.mainQueue()) {
+                
+                (data: CMGyroData!, error: NSError!) in
+                
+                //println("x: \(data.rotationRate.x) y: \(data.rotationRate.y) z: \(data.rotationRate.z)")
+                
+                return
+            }
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
